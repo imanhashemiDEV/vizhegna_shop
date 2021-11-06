a24842bf1c9f9c1af688f289ba412a7926b7011c
iman hashemi <hashemi.iman@gmail.com>
2021-11-06 13:17:23 +0330

    style

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -15,9 +15,7 @@
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                     data-accordion="false">
-                    <!-- Add icons to the links using the .nav-icon class
-                       with font-awesome or any other icon font library -->
-                    <li class="nav-item has-treeview menu-open">
+                    <li class="nav-item has-treeview">
                         <a href="#" class="nav-link active">
                             <i class="nav-icon fa fa-folder"></i>
                             <p>
@@ -40,8 +38,7 @@
                             </li>
                         </ul>
                     </li>
-
-                    <li class="nav-item has-treeview menu-open">
+                    <li class="nav-item has-treeview">
                         <a href="#" class="nav-link active">
                             <i class="nav-icon fa fa-folder"></i>
                             <p>
@@ -64,8 +61,7 @@
                             </li>
                         </ul>
                     </li>
-
-                    <li class="nav-item has-treeview menu-open">
+                    <li class="nav-item has-treeview">
                         <a href="#" class="nav-link active">
                             <i class="nav-icon fa fa-folder"></i>
                             <p>
@@ -88,8 +84,7 @@
                             </li>
                         </ul>
                     </li>
-
-                    <li class="nav-item has-treeview menu-open">
+                    <li class="nav-item has-treeview">
                         <a href="#" class="nav-link active">
                             <i class="nav-icon fa fa-folder"></i>
                             <p>
@@ -112,7 +107,7 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="nav-item has-treeview menu-open">
+                    <li class="nav-item has-treeview">
                         <a href="#" class="nav-link active">
                             <i class="nav-icon fa fa-folder"></i>
                             <p>
