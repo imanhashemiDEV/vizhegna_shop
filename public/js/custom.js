7ba804b7da71332d0d38d0873211d15d7d2a68de
iman <hashemi.iman@gmail.com>
2022-01-03 17:54:50 +0330

    breadcrump

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,3 +2,26 @@
 let url="http://127.0.0.1:8000";
 
 
+$('.breadcrumbs li a').each(function(){
+
+    var breadWidth = $(this).width();
+
+    if($(this).parent('li').hasClass('active') || $(this).parent('li').hasClass('first')){
+
+
+
+    } else {
+
+        $(this).css('width', 75 + 'px');
+
+        $(this).mouseover(function(){
+            $(this).css('width', breadWidth + 'px');
+        });
+
+        $(this).mouseout(function(){
+            $(this).css('width', 75 + 'px');
+        });
+    }
+
+
+});
--- resources/views/admin/partials/breadcrump.blade.php
+++ resources/views/admin/partials/breadcrump.blade.php
@@ -12,3 +12,18 @@
     </div><!-- /.container-fluid -->
 </div>
 <!-- /.content-header -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <ul class="breadcrumbs">
+                    <li class="first"><a href="#" class="icon-home"></a></li>
+                    <li><a href="#">First Level Interior Page</a></li>
+                    <li><a href="#">Second Level Interior Page</a></li>
+                    <li><a href="#">Third Level Interior Page</a></li>
+                    <li class="last active"><a href="#">Fourth Level Interior Page</a></li>
+                </ul>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
