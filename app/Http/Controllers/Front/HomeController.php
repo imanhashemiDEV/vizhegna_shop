0d51368dc6d18f0a56868d320d9cdeeaae7bdb88
iman hashemi <hashemi.iman@gmail.com>
2022-01-18 14:20:41 +0330

    localization

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        app()->setLocale('en');
+//        $users = User::query()->
+//            whereHas('articles',function ($q){
+//                $q->where('status',1);
+//        })
+//            ->get();
+//
+//
+//        dd($users);
+        //app()->setLocale('en');
 
         $categories = Category::query()->where('parent_id',0)->get();
         return view('front.index',compact('categories'));
